3e0bd3bb9fbf448d166c1f902850f2467c770476
Nicholas Gunawan <nicholasgunawan80@gmail.com>
2025-09-29 00:42:10 +0700

    Buat model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'price', 'stock', 'category_id', 'is_active'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function cartItems()
+    {
+        return $this->hasMany(CartItem::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
