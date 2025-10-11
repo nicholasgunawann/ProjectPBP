11836b97fb6e66f9802bc2a37f8392dc9eb30941
Muhammad Iman <muhammad.imansasongko@gmail.com>
2025-10-11 22:49:06 +0700

    menambahkan komen ppp pada file queue config

--- config/queue.php
+++ config/queue.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Default Queue Connection Name
+    | Default Queue Connection Name   ppppp
     |--------------------------------------------------------------------------
     |
     | Laravel's queue API supports an assortment of back-ends via a single
