14cfc793984b14540f9c5aa6ceae443077f4ef99
Reii <casishafwanraihan28@users.noreply.github.com>
2025-10-13 18:51:38 +0700

    Ganti logo  toko

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,9 +4,11 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('products.index') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                <div class="flex-shrink-0 flex items-center">
+                    <a href="{{ url('/') }}">
+                        <img src="{{ asset('logo-sixstore.png') }}" 
+                            alt="SixStore Logo" 
+                            class="h-7 w-auto object-contain align-middle">
                     </a>
                 </div>
 
